9de98d9d8c926b5f6ff9a66d5d891e1b1bcb26da
Ryan Olson <rmolson@gmail.com>
2017-03-07 14:50:21 -0700

    if no command is given, then default to bash

--- luda/bootstrap/init.sh
+++ luda/bootstrap/init.sh
@@ -11,4 +11,8 @@ echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" --create-home -G sudo $USER_NAME > /dev/null 2>&1
 export HOME=/home/$USER_NAME
 
-exec /bootstrap/su-exec $USER_NAME "$@"
+if [ ${#@} -eq 0 ]; then
+    exec /bootstrap/su-exec $USER_NAME /bin/bash
+else
+    exec /bootstrap/su-exec $USER_NAME "$@"
+fi
