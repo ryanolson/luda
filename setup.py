2ec219197aebab76fe62bd6c053a9b6be0c1a2f2
Ryan Olson <rmolson@gmail.com>
2017-06-04 17:29:40 -0600

    Bump version: 0.4.3 → 0.4.4

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='luda',
-    version='0.4.3',
+    version='0.4.4',
     description="ludicrously awesome [w]rapper for nvidia-docker",
     long_description=readme + '\n\n' + history,
     author="Ryan Olson",
